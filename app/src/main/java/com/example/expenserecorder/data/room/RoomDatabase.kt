7ed8535413106d4fd85e32e9df4765d0030e3ac6
gecchma1 <mattia.gecchele@reti.it>
2021-09-21 17:02:53 +0200

    init room and base view

--- app/src/main/java/com/example/expenserecorder/data/room/RoomDatabase.kt
+++ app/src/main/java/com/example/expenserecorder/data/room/RoomDatabase.kt
@@ -21,7 +21,7 @@ abstract class Database: RoomDatabase() {
         @JvmField
         val MIGRATION_0_1: Migration = object : Migration(0, 1) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                //Migration tecnica per far si che Room setti il DB pre popolato come un DB room
+                //technical migration
             }
         }
     }
